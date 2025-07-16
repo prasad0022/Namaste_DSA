61a557d1a839a6b1dcc283a62e28863157fa02ae
Prasad <ppkhose6@gmail.com>
2025-07-16 17:17:08 +0530

    Bubble Sort

---
+++ Searching & Sorting/BubbleSort.js
@@ -0,0 +1,28 @@
+
+// Bubble Sort:
+
+const arr = [2, 6, 1, 9, 5];
+
+const bubbleSort = (arr) => {
+    let temp;
+    let n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+
+        let isSwapped = false;
+
+        for (let j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                isSwapped = true;
+            }
+        }
+
+        if (!isSwapped) break;
+    }
+
+    return arr;
+}
+
+console.log(bubbleSort(arr));
